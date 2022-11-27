23223237391e8abb15c966248e460dd33f5bb422
Marco Ruta <marcoruta98@gmail.com>
2022-11-27 12:32:59 +0100

    lsh approach refactoring

--- utility/train_and_test.py
+++ utility/train_and_test.py
@@ -9,6 +9,8 @@ from sklearn.naive_bayes import MultinomialNB
 from prettytable import PrettyTable
 from sklearn.model_selection import cross_val_score, cross_val_predict
 from termcolor import colored
+from utility import stemmer
+from datasketch import MinHash, MinHashLSH
 
 # Classifiers that will be used
 classifiers = [
@@ -56,6 +58,9 @@ def classify(train_x, train_y, test_x, test_y):
         t.add_row( ['', '', '', '', '', ''] )
     print( t )
 
+
+# Function that train and test the classifier with k-fold cross validation and return
+# the results in a fancy table
 def k_fold_cross_validation(x,y,k):
 
     t = PrettyTable(
@@ -76,9 +81,9 @@ def k_fold_cross_validation(x,y,k):
     print(t)
     return
 
-
-
+# Function that evaluate the metrics of a given blacklist over a given testing set
 def evaluate_blacklist(X_test,y_test,blacklist):
+
     tp = 0
     tn = 0
     fp = 0
@@ -100,6 +105,65 @@ def evaluate_blacklist(X_test,y_test,blacklist):
             else:
                 tn += 1
 
+
+    confusion_matrix = [[tn,fp],
+                        [fn,tp]]
+
+
+    accuracy = round((tp+tn)/(tp + fp + tn + fn), 3)
+    precision = round((tp /(tp + fp)), 3)
+    recall = round((tp / (tp + fn)), 3)
+    f1 = round(((2*precision*recall)/(precision + recall)), 3)
+
+    t = PrettyTable(
+            ['Confusion Matrix', 'Accuracy', 'Precision', 'Recall', 'F1'] )
+
+    t.add_row([confusion_matrix, accuracy, precision, recall, f1 
+            ])
+
+    t.add_row( ['', '', '', '', ''] )
+    print( t )
+
+# Function that evaluate the metrics of a given lsh on a given testing set
+def lsh_classify(X_test,y_test,lsh):
+
+    def lsh_predict_label(stems):
+
+        minhash = MinHash(num_perm=128)
+        if len(stems) < 2:
+            return -1
+        for s in stems:
+            minhash.update(s.encode('utf-8'))
+        matches = lsh.query(minhash)
+        if matches:
+            return 1
+        else:
+            return 0
+
+
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+
+    for text,label in zip(X_test,y_test):
+        stems = stemmer.stem(text)
+        pred = lsh_predict_label(stems)
+        # parsing error
+        if pred == -1:
+            continue
+        # predicted spam
+        elif pred == 1: 
+            if label:
+                tp += 1
+            else:
+                fp += 1 
+        # predicted ham
+        elif pred == 0: 
+            if label == 1:
+                fn += 1
+            else:
+                tn += 1
 
     confusion_matrix = [[tn,fp],
                         [fn,tp]]
