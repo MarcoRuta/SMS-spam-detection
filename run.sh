39bec3478c12a4cdecdeb738842b2fc52f9e178c
Marco Ruta <marcoruta98@gmail.com>
2022-11-27 16:22:25 +0100

    Added required packages auto-install

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
+pip install -Ur required_packages.txt
 python3 utility/data_analysis.py
 echo -e "\e[1;31m Blacklist approach \e[0m"
 python3 blacklist.py
 echo -e "\e[1;31m LSH approach \e[0m"
 python3 locality_sensitive_hashing.py
-echo -e "\e[1;31m CountVectorizer approach \e[0m"
-python3 count_vectorizer.py
-
