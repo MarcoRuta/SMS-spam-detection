6558ad8ecd09022dd1c607cdefe619987fd11dd9
Marco Ruta <marcoruta98@gmail.com>
2022-12-07 15:21:24 +0100

    Store of the trained algorithms added

--- count_vectorizer.py
+++ count_vectorizer.py
@@ -1,6 +1,5 @@
 # importing all the needed packages
 from utility import data_preprocessing 
-from utility import train_and_test
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 from sklearn.linear_model import SGDClassifier
@@ -8,6 +7,7 @@ from sklearn.naive_bayes import MultinomialNB
 from prettytable import PrettyTable
 from sklearn.model_selection import cross_val_score
 from termcolor import colored
+import pickle
 
 
 # Classifiers that will be used
@@ -41,6 +41,10 @@ def classify(train_x, train_y, test_x, test_y):
         _recall = format(recall_score(test_y,predictions))
         _f1 = format(f1_score(test_y,predictions))
         _matrix = confusion_matrix(test_y,predictions)
+
+        # saving the trained algorithm in the trained_models directory
+        filename = 'trained_models/'+name+'.sav'
+        pickle.dump(clf, open(filename, 'wb'))
 
         t.add_row(
             [colored( name, 'blue' ), _matrix, _accuracy[0:5], _precision[0:5], _recall[0:5], _f1[0:5] 
@@ -102,8 +106,8 @@ for vectorizer,features in zip(count_vectorizers, vectorizer_features):
 
     # classify with 70 % training and 30% testing
     print("\n\n"+"*"*60+"\nResults for vectorizer: "+features+"\n"+"*"*60+"\n\n")
-    train_and_test.classify(training_data,y_train,testing_data,y_test)
+    classify(training_data,y_train,testing_data,y_test)
 
 # classify with k-cross fold validation (k = 10)
 full_data = count_vectorizers[-1].transform(X)
-train_and_test.k_fold_cross_validation(full_data,Y,8)
+k_fold_cross_validation(full_data,Y,8)
