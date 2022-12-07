9513dabb177f1decea0100d0692ebde6a569a7e2
Marco Ruta <marcoruta98@gmail.com>
2022-12-07 18:21:53 +0100

    minor changes

--- run.sh
+++ run.sh
@@ -3,4 +3,7 @@ echo -e "\e[1;31m Blacklist approach \e[0m"
 python3 blacklist.py
 echo -e "\e[1;31m LSH approach \e[0m"
 python3 locality_sensitive_hashing.py
+echo -e "\e[1;31m Count Vectorizer approach \e[0m"
 python3 count_vectorizer.py
+echo -e "\e[1;31m Starting the web app! \e[0m"
+python3 UI.py
