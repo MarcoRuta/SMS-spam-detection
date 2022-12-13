a59c7d3006584ff6c1551c51e72cacf9a987dd11
Marco Ruta <marcoruta98@gmail.com>
2022-12-13 11:28:42 +0100

    auto flagging in the web app

--- UI.py
+++ UI.py
@@ -11,8 +11,8 @@ def spam_checker(sms):
     loaded_vectorizer = pickle.load(open('trained_models/vectorizer.pickle', 'rb'))
 
     # load the models
-    loaded_model_NB = pickle.load(open('trained_models/Naive Bayes.model', 'rb'))
-    loaded_model_SVM = pickle.load(open('trained_models/SGD SVM.model', 'rb'))
+    loaded_model_NB = pickle.load(open('trained_models/Multinomial Naive Bayes.model', 'rb'))
+    loaded_model_SVM = pickle.load(open('trained_models/SVC.model', 'rb'))
 
     # making a prediction with both the algorithms
     result_NB = loaded_model_NB.predict(loaded_vectorizer.transform(sms))
@@ -23,4 +23,9 @@ def spam_checker(sms):
     else: return "This SMS seems legit!"
 
 
-gr.Interface(fn=spam_checker, inputs=["text"], outputs=["text"]).launch(share = True)
+gr.Interface(fn=spam_checker, 
+            title="SMS spam checker", 
+            allow_flagging='auto', 
+            inputs=gr.Textbox(placeholder="Insert here the SMS"), 
+            outputs=gr.Textbox(placeholder="The result will be displayed here")
+            ).launch(share = True)
